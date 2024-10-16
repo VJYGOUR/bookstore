98e74a4e3cba91681f0a36f1a5d6f0e09fc14c43
vijay kumar <kumarvijayy036@gmail.com>
2024-10-16 13:40:48 +0530

    addSidebar

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -3,7 +3,11 @@ import { RxHamburgerMenu } from "react-icons/rx";
 function Logo({ setOpen }) {
   return (
     <div className="flex gap-3 items-center">
-      <RxHamburgerMenu onClick={() => setOpen(true)} className="sm:hidden" />
+      <RxHamburgerMenu
+        size={25}
+        onClick={() => setOpen(true)}
+        className="sm:hidden"
+      />
       <img src="logo.cd9e58e4.svg" alt="logo" className="h-12" />
     </div>
   );
